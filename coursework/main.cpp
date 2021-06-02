7cf1e190e66294e32aafadff0b37cc53c9b253fa
Pop-Os <aevaparracarlos@gmail.com>
2021-06-03 02:13:42 +0300

    Main, with a random number generator wich automates the creation of the classes

--- coursework/main.cpp
+++ coursework/main.cpp
@@ -1,27 +1,32 @@
 #include <iostream>
+#include <time.h>
+#include <stdlib.h>
 #include "integer.hpp"
 
 int main(int argc, char const *argv[]) {
-  integer A(1, 2); // (a, b)
-  integer B(3, 9); // (c, d)
+  srand (time(NULL));
+
+  integer A(rand()% 5+1, rand()% 5+1); // (a, b)
+  integer B(rand()% 5+1, rand()% 5+1); // (c, d)
   integer C = A + B;
   integer D = A - B;
+  bool E = A == B;
+  bool F = A != B;
+  bool G = A >= B;
+  bool H = A <= B;
+  bool I = A > B;
+  bool J = A < B;
 
   std::vector<unsigned> A_equiv_class = A.GetEquivClass();
   std::vector<unsigned> B_equiv_class = B.GetEquivClass();
   std::vector<unsigned> C_equiv_class = C.GetEquivClass();
   std::vector<unsigned> D_equiv_class = D.GetEquivClass();
 
+
   std::cout
   << "A: (" << A_equiv_class[0] << ", " << A_equiv_class[1] << ")" << '\n'
   << "B: (" << B_equiv_class[0] << ", " << B_equiv_class[1] << ")" << '\n'
   << "C: (" << C_equiv_class[0] << ", " << C_equiv_class[1] << ")" << '\n'
   << "D: (" << D_equiv_class[0] << ", " << D_equiv_class[1] << ")" << '\n';
-
-  int a = -1;
-  int b = -3;
-
-  std::cout << "a: " << a << "\nb: " << b << "\na+b: " << a+b << '\n';
-
   return 0;
 }
